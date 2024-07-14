ba90bf0598d98e656ab6711a950c0dcf14f5d23d
gbetting <gbetting@student.42.fr>
2024-07-14 22:11:12 +0200

    almost finished sh script

--- monitoring.sh
+++ monitoring.sh
@@ -5,14 +5,14 @@ ARCHITECTURE=$(uname -a)
 CPU_PHYSICAL=$(lscpu | head -n 5 | tail -n 1 | awk '{print $NF}')
 CPU_VIRTUAL=$(nproc)
 
-RAM_TOTAL=$(free -m | head -n 2 | tail -n 1 | awk '{print $2}')
-RAM_USED=$(free -m | head -n 2 | tail -n 1 | awk '{print $3}')
+RAM_TOTAL=$(free -m | head -n 2 | tail -n 1 | awk '{printf "%.0f", $2*1000000}')
+RAM_USED=$(free -m | head -n 2 | tail -n 1 | awk '{printf "%.0f", $3*1000000}')
 RAM_PERCENT=$(echo "scale=1; $RAM_USED / $RAM_TOTAL * 100" | bc)
+RAM_TOTAL=$(echo "$RAM_TOTAL" | numfmt --to=iec-i --suffix=B)
+RAM_USED=$(echo "$RAM_USED" | numfmt --to=iec-i --suffix=B)
 
-DISK_TOTAL_root=$(df | head -n 4 | tail -n 1 | awk '{print $3+$4}')
-DIST_TOTAL_home=$(df | head -n 7 | tail -n 1 | awk '{print $3+$4}')
-DISK_TOTAL=$(echo "$DISK_TOTAL_root + $DISK_TOTAL_home" | bc)
-DISK_USED=$(df | head -n 4 | tail -n 1 | awk '{print $3}')
+DISK_TOTAL=$(df | grep -E '/$|/home$|/dev$' | awk '{sum+=$2*1000} END {printf "%.0f\n", sum}')
+DISK_USED=$(df | grep -E '/$|/home$|/dev$' | awk '{sum+=$3*1000} END {printf "%.0f\n", sum}')
 DISK_PERCENT=$(echo "scale=1; $DISK_USED / $DISK_TOTAL * 100" | bc)
 DISK_TOTAL=$(echo "$DISK_TOTAL" | numfmt --to=iec-i --suffix=B)
 DISK_USED=$(echo "$DISK_USED" | numfmt --to=iec-i --suffix=B)
@@ -28,19 +28,20 @@ TCP_CONN=$(echo "`ss -tunlp | wc -l` - 1" | bc)
 
 USER_LOG=$(who | wc -l)
 
-# IP=$(hostname -I awk '{print $1}')
-IP=""
+IP=$(hostname -I | awk '{print $1}')
 # MAC=$(ip a | grep "ether" | awk '{print $2}')
-MAC=""
+MAC=$(ip a | grep ether | head -n 1 | awk '{print $2}')
 
-#SUDO_CMD=$(cat /var/log/sudo/sudo.log | wc -l)
-SUDO_CMD=""
+
+#TODO : where is the sudo.log file ????
+SUDO_CMD=$(cat /var/log/sudo/logs | wc -l)
+# SUDO_CMD=""
 
 
 printf "\t#Architecture: $ARCHITECTURE\n"
 printf "\t#CPU physical: $CPU_PHYSICAL\n"
 printf "\t#vCPU: $CPU_VIRTUAL\n"
-printf "\t#RAM: $RAM_USED/$RAM_TOTAL MB ($RAM_PERCENT%%)\n"
+printf "\t#RAM: $RAM_USED/$RAM_TOTAL ($RAM_PERCENT%%)\n"
 printf "\t#Disk: $DISK_USED/$DISK_TOTAL ($DISK_PERCENT%%)\n"
 printf "\t#CPU load: $CPU_LOAD%%\n"
 printf "\t#Last boot: $LAST_BOOT\n"
